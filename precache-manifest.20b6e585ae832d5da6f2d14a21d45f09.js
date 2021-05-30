836f452d374a7e9c455a3125da6b6ac439dc50f1
Thomas <thomas.trinque@gmail.com>
2021-05-30 23:08:05 +0200

    Updates

---
+++ precache-manifest.20b6e585ae832d5da6f2d14a21d45f09.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27eeb15d8708efd319006d52bd01befd",
+    "url": "/bet/index.html"
+  },
+  {
+    "revision": "4663cca5d798636e92f6",
+    "url": "/bet/static/css/2.6966f163.chunk.css"
+  },
+  {
+    "revision": "716aee548fc1f1de7263",
+    "url": "/bet/static/css/main.e6db22dd.chunk.css"
+  },
+  {
+    "revision": "4663cca5d798636e92f6",
+    "url": "/bet/static/js/2.ca5e0a52.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/bet/static/js/2.ca5e0a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716aee548fc1f1de7263",
+    "url": "/bet/static/js/main.6c1f30ff.chunk.js"
+  },
+  {
+    "revision": "4c8c022df1efe8cb58a7",
+    "url": "/bet/static/js/runtime-main.d9f87587.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/bet/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/bet/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/bet/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/bet/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/bet/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bet/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/bet/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/bet/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/bet/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/bet/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/bet/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/bet/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/bet/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/bet/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/bet/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/bet/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
